a511dc6bd437fcf54a720f66f36110f4beeefe2d
aSilentInferno <aSilentInferno@outlook.com>
2024-08-23 15:31:38 +0200

    added input handelig

---
+++ SpongeBobMockingTextGen/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SpongebobMockingText
+{
+    internal class Program
+    {
+        public static void Main(String[] args)
+        {
+    
+        }
+
+        internal static string getLowerCaseInput()
+        {
+            Console.WriteLine("please enter the text");
+            return Console.ReadLine().ToLower();
+        }
+
+    }
+}
